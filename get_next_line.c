b8fb19501710523fe85c5378acce0c5598a4cdc1
Oleksii Ishchenko <oishchen@1-C-8.42heilbronn.de>
2025-03-31 17:43:16 +0200

    version pre final, where i have multiple free and read errors

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,103 @@
 /*   By: oishchen <oishchen@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 13:33:28 by oishchen          #+#    #+#             */
-/*   Updated: 2025/03/28 06:08:37 by oishchen         ###   ########.fr       */
+/*   Updated: 2025/03/31 17:42:59 by oishchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-	/*
-	inits: buf[1024][BUFFER_MAX];
-	*/
-	/*
-	main logic: take the buffer into the read by the fd, 
-	store there the chars by the size of BUFFER_MAX,
-		use a strdup for this purpose that should also take a flga of the found_nl
-	join the line with some temp string
-	*/
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	if (!s)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	uc;
+
+	uc = (unsigned char)c;
+	while (*s)
+	{
+		if (*s == uc)
+			return ((char *)s);
+		s++;
+	}
+	if (uc == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+void	*ft_memcpy(void *to, const void *from, size_t len)
+{
+	unsigned char	*ptr;
+	unsigned char	*src_cpy;
+
+	if (!to && !from)
+		return (NULL);
+	ptr = (unsigned char *)to;
+	src_cpy = (unsigned char *)from;
+	while (len--)
+		*ptr++ = *src_cpy++;
+	return (to);
+}
 
 char	*ft_strdup(const char *s1)
 {
-	int		i;
+	int		s1len;
 	char	*dup;
 
-	i = 0;
-	while (s1[i])
-		i++;
-	dup = (char *)malloc(sizeof(char) * (i + 1));
+	s1len = ft_strlen(s1);
+	dup = (char *)malloc(sizeof(char) * (s1len + 1));
 	if (!dup)
 		return (NULL);
-	dup[i] = '\0';
-	while (i--)
-		dup[i] = s1[i];
+	dup = ft_memcpy(dup, s1, s1len);
+	dup[s1len] = '\0';
 	return (dup);
 }
 
-int	gnl_len(char *str)
+char	*ft_strjoin(char const *container, char const *buf)
 {
-	int	i;
-
-	if (!str || *str == '\0')
-		return (0);
-	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	return (i);
-}
-
-char	*gnl_join(char *buf, char *buf_storage, int *bytes_joined)
-{
+	int		buf_len;
+	int		container_len;
 	char	*joined;
-	int		s1len;
-	int		s2len;
-	int		i;
 
-	s1len = gnl_len(buf_storage);
-	s2len = gnl_len(buf);
-
-	joined = (char *)malloc((s1len + s2len + 1) * sizeof(char));
+	buf_len = ft_strlen(buf);
+	container_len = ft_strlen(container);
+	joined = (char *)malloc(sizeof(char) * (buf_len + container_len + 1));
 	if (!joined)
 		return (NULL);
-	i = 0;
-	while (i < s1len)
-	{
-		joined[i] = buf_storage[i];
-		i++;
-	}
-	i = 0;
-	while (i - s1len < s2len)
-	{
-		joined[i + s1len] = buf[i];
-		i++;
-	}
-	joined[i] = '\0';
-	printf("Here is our joined: %s\n", joined);
-	*bytes_joined = s1len + s2len;
-	printf("The Bytes_joined: %d\n", *bytes_joined);
+	ft_memcpy(joined, container, container_len);
+	ft_memcpy(joined + container_len, buf, buf_len);
+	joined[buf_len + container_len] = '\0';
 	return (joined);
+}
+
+char	*get_pure_line(char *container)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	while (container[i] && container[i] != '\n')
+		i++;
+	if (container[i] == '\n')
+		i++;
+	result = (char *)malloc((i + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	result[i] = '\0';
+	while (i--)
+		result[i] = container[i];
+	return (result);
 }
 
 char	*reset_buf(char *buf)
@@ -94,74 +113,113 @@ char	*reset_buf(char *buf)
 
 	i = 0;
 	j = 0;
-	printf("we are in reset_buf funciton\n");
 	cpy_buf = buf;
-	while (cpy_buf[i] != '\n')
+	while (i < BUFFER_SIZE && cpy_buf[i] != '\n')
 		i++;
 	i++;
-	while (cpy_buf[i])
+	if (cpy_buf[i - 1] == '\n')
 	{
-		buf[j] = cpy_buf[i];
-		j++;
-		i++;
+		while (i < BUFFER_SIZE)
+		{
+			buf[j] = cpy_buf[i];
+			j++;
+			i++;
+		}
 	}
-	buf[j] = '\0';
+	while (j < BUFFER_SIZE)
+	{
+		buf[j] = '\0';
+		j++;
+	}
 	return (buf);
 }
 
-char	*ft_fetch_word(char *buf, int fd)
+char	*buf_free(char *buf)
 {
-	char	*buf_storage;
-	int		bytes_read;
-	int		is_nl_found;
-	int		bytes_joined;
+	int	i;
 
-	buf_storage = ft_strdup(buf);
-	printf("So far buf_storage looks like : %s\n", buf_storage);
-	is_nl_found = 0;
-	while (is_nl_found != 1)
+	i = 0;
+	if (buf)
 	{
-		printf("We are in the loop\n");
-		bytes_read = read(fd, buf, BUFFER_SIZE);
-		printf("bytes read: %d\n", bytes_read);
-		if (bytes_read <= 0)
-			return (NULL);
-		buf[bytes_read] = '\0';
-		printf("So far buf looks like : %s\n", buf);
-		buf_storage = gnl_join(buf, buf_storage, &bytes_joined);
-		if (buf_storage[bytes_joined - 1] == '\n')
+		while (i < BUFFER_SIZE)
 		{
-			printf("the char buf_storage[bytes_joined - 1]: %c\n", buf_storage[bytes_joined - 1]);
-			is_nl_found = 1;
-			// buf = reset_buf(buf);
-			// printf("buf after reset: %s\n", buf);
+			buf[i] = '\0';
+			i++;
 		}
 	}
-	return (buf_storage);
+	return (buf);
+}
+
+char	*get_nl_line(char *buf, char *container, int fd)
+{
+	int		read_bytes;
+	char	*joined;
+	char	*result;
+
+	read_bytes = BUFFER_SIZE;
+	// printf("the container %s and its len: %lu\n", container, strlen(container));
+	while (read_bytes == BUFFER_SIZE && ft_strchr(container, '\n') == NULL)
+	{
+		read_bytes = read(fd, buf, BUFFER_SIZE);
+		if (read_bytes == -1)
+		return (free(container), buf_free(buf), NULL);
+		// printf("Bytes read: %d\n", read_bytes);
+		buf[read_bytes] = '\0';
+		// printf("THE CUR_BUF: %s\n", buf);
+		joined = ft_strjoin(container, buf);
+		if (!joined)
+			return (free(container), NULL);
+		// printf("THE joined: %s\n", joined);
+		free(container);
+		container = joined;
+	}
+	if (read_bytes == -1)
+		buf_free(buf);
+	// printf("the buf %s and its len: %lu\n", buf, strlen(buf));
+	result = get_pure_line(container);
+	if (!result)
+		return (NULL);
+	// printf("SB RESULT: %s\n", result);
+	reset_buf(buf);
+	// printf("the reset_buf %s and its len: %lu\n", buf, strlen(buf));
+	free(container);
+	return (result);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	buf[1024][BUFFER_SIZE + 1];
+	char	*result;
 	char	*line;
 
-	if (BUFFER_SIZE < 0 || fd < 0)
-		return (NULL);
-	line = ft_fetch_word(buf[fd], fd);
+	if (read(fd, buf[fd], 0) < 0 || BUFFER_SIZE <= 0 || fd < 0)
+		return (memset(buf[fd], '\0', ft_strlen(buf[fd])), NULL);
+	line = ft_strdup(buf[fd]);
 	if (!line)
 		return (NULL);
-	reset_buf(buf[fd]);
-	printf("buf after reset: %s\n", buf[fd]);
-	return (line);
+	result = get_nl_line(buf[fd], line, fd);
+	if (!result)
+		return (free(line), NULL);
+	if (result[0] == '\0' && ft_strlen(result) == 0)
+		return (free(result), NULL);
+	return (result);
 }
-int main()
-{
-	int fd = open("test.txt", O_RDWR);
-	char	*temp = get_next_line(fd);
-	printf("RESULT: %s",temp);
-	free(temp);
-	temp = get_next_line(fd);
-	printf("RESULT: %s",temp);
-	free(temp);
-	return (0);
-}
+
+// int main()
+// {
+// 	// int fd = open("test.txt", O_RDWR);
+// 	int fd1 = open("test1.txt", O_RDWR);
+// 	int i = 0;
+// 	char	*temp;
+// 	// printf("RESULT: %s",temp);
+// 	// free(temp);
+// 	while (i < 5)
+// 	{
+// 		temp = get_next_line(fd1);
+// 		printf("RESULT: %s", temp);
+// 		if (temp)
+// 			free(temp);
+// 		i++;
+// 	}
+// 	return (0);
+// }
